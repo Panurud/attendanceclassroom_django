8145427380abbaa0d8273b7b1a31814060907641
Panurud <panurud.k@gmail.com>
2024-03-04 23:32:09 +0700

    [Set] Index Page

--- attendanceclassroom_django/settings.py
+++ attendanceclassroom_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-73p+6ul$2z6fvvr)y397ncx3vl71vtt4pe4e5%caozc$tlp5kx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'attendance_classroom'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'attendanceclassroom_django.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
