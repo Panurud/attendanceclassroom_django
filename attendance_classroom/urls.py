01b046464767384b1b2782526ca84fe3ddc7f823
Panurud <panurud.k@gmail.com>
2024-03-05 01:03:27 +0700

    [Add] Login Page

--- attendance_classroom/urls.py
+++ attendance_classroom/urls.py
@@ -1,7 +1,8 @@
 
 from django.urls import path 
-from attendance_classroom.views import index
+from attendance_classroom.views import index , signin
 
 urlpatterns = [
-    path('',index)
+    path('',index),
+    path('signin',signin)
 ]
--- attendance_classroom/views.py
+++ attendance_classroom/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from django.contrib.auth import login , logout
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request,"login/login.html")
+
+def signin(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        print(f'User:{username} | Password:{password}')
+        user = authenticate(request, username=username, password=password)
+        print("User:",user)
+        if user is not None:
+            # Log user in
+            login(request, user)
+            return render(request,"login/home.html")
+        else : 
+            logout(request)
+    return redirect('/')
